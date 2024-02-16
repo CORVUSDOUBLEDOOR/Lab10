0c7b10becb05cbd5962a92f10f89725ab6e505d5
CORVUS-DOUBLEDOOR <153846979+CORVUSDOUBLEDOOR@users.noreply.github.com>
2024-02-16 15:35:01 +0700

    Update Tester.java

--- Lab10Ex4/Tester.java
+++ Lab10Ex4/Tester.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Tester {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner newFile = new Scanner(new File("C:\\Users\\ghost\\IdeaProjects\\untitled\\src\\Lab10Ex4\\text.txt"));
+        Scanner newFile = new Scanner(new File("C:\\Users\\informatics\\IdeaProjects\\lap8\\src\\Lab10Ex4\\text.txt"));
         ArrayList<String> sentences = new ArrayList<String>();
         while(newFile.hasNextLine()){
             sentences.add(newFile.nextLine());
         }
         for(int i = 0; i < sentences.size() ; i++){
-
+            StringBuilder reverseSentences = new StringBuilder(sentences.get(i)).reverse();
+            String senten = sentences.get(i);
+            System.out.println(reverseSentences);
         }
     }
 }
